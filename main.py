8c62be289f4bad47d2476c4df717dcbf795450ec
surperxsj <1321411003@qq.com>
2019-07-31 10:45:53 +0800

    Cnn first code

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import struct
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import confusion_matrix
+
+import time
+from datetime import timedelta
+import math
